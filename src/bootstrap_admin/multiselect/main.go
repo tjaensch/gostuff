10410d732a493e458bd3ab1fc1d5cb83458e295a
Thomas Jaensch <thomas.jaensch@noaa.gov>
2016-10-31 13:48:33 +0000

    Added form select to bootstrap_admin/multiselect

---
+++ src/bootstrap_admin/multiselect/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "net/http"
+)
+
+func myHandler(w http.ResponseWriter, r *http.Request) {
+    if r.Method == "POST" {
+        // Form submitted
+        r.ParseForm() // Required if you don't call r.FormValue()
+        fmt.Println(r.Form["ratings"])
+    }
+    html_template, _ := ioutil.ReadFile("html_template.html")
+    w.Write([]byte(html_template))
+}
+
+func main() {
+    fs := http.FileServer(http.Dir("static"))
+    http.Handle("/static/", http.StripPrefix("/static/", fs))
+    http.HandleFunc("/", myHandler)
+    http.ListenAndServe("10.90.235.15:1313", nil)
+}
