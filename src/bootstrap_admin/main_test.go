931533375605c01ac48839b272952c3c5f9945e4
Thomas Jaensch <thomas.jaensch@noaa.gov>
2016-10-31 14:59:37 +0000

    added http handler tests to bootstrap_admin

---
+++ src/bootstrap_admin/main_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+  "net/http/httptest"
+  "strings"
+	"testing"
+)
+
+func TestDsmmRatings(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+  rr := httptest.NewRecorder()
+  handler := http.HandlerFunc(dsmmRatings)
+
+  handler.ServeHTTP(rr, req)
+
+  if status := rr.Code; status != http.StatusOK {
+    t.Errorf("handler returned wrong status code: got %v, want %v", status, http.StatusOK)
+  }
+
+  if !strings.Contains(rr.Body.String(), "<label>Preservability</label>") {
+    t.Errorf("handler returned unexpected body")
+  }
+}
