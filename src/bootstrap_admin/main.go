753d9ec1cf40bf20269b959eae53bf04044ba46d
Thomas Jaensch <thomas.jaensch@noaa.gov>
2016-11-01 19:22:54 +0000

    Added form tests to dsmm_gui

---
+++ src/bootstrap_admin/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"text/template"
+
+	"github.com/gorilla/schema"
+)
+
+var (
+	decoder       = schema.NewDecoder()
+	ratingsValues = new(RatingsValues)
+)
+
+type RatingsValues struct {
+	Preservability               string `schema:"preservability"`
+	Accessibility                string `schema:"accessibility"`
+	Usability                    string `schema:"usability"`
+	ProductionSustainability     string `schema:"production_sustainability"`
+	DataQualityAssurance         string `schema:"data_quality_assurance"`
+	DataQualityControlMonitoring string `schema:"data_quality_control_monitoring"`
+	DataQualityAssessment        string `schema:"data_quality_assessment"`
+	TransparencyTraceability     string `schema:"transparency_traceability"`
+	DataIntegrity                string `schema:"data_integrity"`
+}
+
+// Generic error checking function
+func checkError(reason string, err error) {
+	if err != nil {
+		fmt.Printf("%s: %s\n", reason, err)
+		os.Exit(1)
+	}
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/", getDsmmRatings)
+	http.ListenAndServe("10.90.235.15:1313", nil)
+}
+
+func getDsmmRatings(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		// Form submitted
+		err := r.ParseForm() // required if no r.FormValue()
+		checkError("parsing html form failed, program exiting", err)
+
+		err = decoder.Decode(ratingsValues, r.PostForm)
+		checkError("decode form failed, program exiting", err)
+
+    // Parse struct values from HTML form into dsmm_template.tmpl
+		t, err := template.ParseFiles("dsmm_template.tmpl")
+		t.ExecuteTemplate(os.Stdout, "dsmm", ratingsValues)
+		checkError("parsing template failed, program exiting", err)
+		fmt.Println()
+
+	}
+	html_template, _ := ioutil.ReadFile("html_template.html")
+	w.Write([]byte(html_template))
+}
--- src/dsmm_gui/main_test.go
+++ src/dsmm_gui/main_test.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"net/http"
-  "net/http/httptest"
-  "strings"
+	"net/http/httptest"
+	"net/url"
+	"reflect"
+	"strings"
 	"testing"
 )
+
+var initialFormItems map[string]string = map[string]string{
+	"Preservability":               "adhoc",
+	"Accessibility":                "minimal",
+	"Usability":                    "intermediate",
+	"ProductionSustainability":     "advanced",
+	"DataQualityAssurance":         "optimal",
+	"DataQualityControlMonitoring": "adhoc",
+	"DataQualityAssessment":        "minimal",
+	"TransparencyTraceability":     "minimal",
+	"DataIntegrity":                "optimal",
+}
 
 func TestGetDsmmRatings(t *testing.T) {
 	req, err := http.NewRequest("GET", "/", nil)
@@ -13,16 +27,40 @@ func TestGetDsmmRatings(t *testing.T) {
 		t.Fatal(err)
 	}
 
-  rr := httptest.NewRecorder()
-  handler := http.HandlerFunc(getDsmmRatings)
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(getDsmmRatings)
 
-  handler.ServeHTTP(rr, req)
+	handler.ServeHTTP(rr, req)
 
-  if status := rr.Code; status != http.StatusOK {
-    t.Errorf("dsmmRatings handler returned wrong status code: got %v, want %v", status, http.StatusOK)
-  }
+	// Test server response
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("dsmmRatings handler returned wrong status code: got %v, want %v", status, http.StatusOK)
+	}
 
-  if !strings.Contains(rr.Body.String(), "<label>Preservability</label>") {
-    t.Errorf("dsmmRatings handler returned unexpected body")
-  }
+	// Test if html template is returned properly
+	if !strings.Contains(rr.Body.String(), "<label>Preservability</label>") {
+		t.Errorf("dsmmRatings handler returned unexpected body")
+	}
+
+	// Test if form entries are processed correctly
+	req.Form = make(url.Values)
+	for k, v := range initialFormItems {
+		req.Form.Add(k, v)
+	}
+
+	wantForm := url.Values{
+		"Preservability":               []string{"adhoc"},
+		"Accessibility":                []string{"minimal"},
+		"Usability":                    []string{"intermediate"},
+		"ProductionSustainability":     []string{"advanced"},
+		"DataQualityAssurance":         []string{"optimal"},
+		"DataQualityControlMonitoring": []string{"adhoc"},
+		"DataQualityAssessment":        []string{"minimal"},
+		"TransparencyTraceability":     []string{"minimal"},
+		"DataIntegrity":                []string{"optimal"},
+	}
+	if !reflect.DeepEqual(req.Form, wantForm) {
+		t.Fatalf("req.Form = %v, want %v", req.Form, wantForm)
+	}
+
 }
